404e14e9e993a4ada2ef2b83ff4f1fa4bd926cf3
Allan Casa <allan@startstudio.com.br>
2019-05-26 20:47:58 -0300

    primeiro

---
+++ bootstrap.php
@@ -0,0 +1,29 @@
+<?php 
+
+require __DIR__.'/vendor/autoload.php';
+
+$router = new SON\Framework\Router;
+
+require __DIR__ . '/config/containers.php';
+require __DIR__ . '/config/routes.php';
+
+try {
+    $result = $router->run();
+    
+    $response = new SON\Framework\Response;
+    
+    $params = [
+        'container' => $container,
+        'params' => $result['params']
+    ];
+    
+    $response($result['action'], $params);
+
+ 
+    
+
+} catch (\SON\Framework\Exceptions\HttpException $e) {
+    echo json_encode(['error' => $e->getMessage()]);
+}
+
+
---
+++ config/routes.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+
+$router->add('GET','/', function () {  
+    return 'estamos na homepage';
+});
+
+$router->add('GET','/users/(\d+)', '\App\Controllers\UserController::show');
